2e85ef93d6625bc780461e41e0090195ad3e0d1b
sang105 <15791755+sang105@users.noreply.github.com>
2026-03-20 12:45:12 +0000

    Updated favicon import

--- app/root.tsx
+++ app/root.tsx
@@ -29,7 +29,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <link rel="icon" href="public/favicon.ico" />
+        <link rel="icon" href={`${import.meta.env.BASE_URL}favicon.ico`} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <script>
           var pathSegmentsToKeep = 1; // keep the repo name segment
